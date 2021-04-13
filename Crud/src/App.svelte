dfcfeb44dc399bf51ad563f9a718d0476b6c50a0
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-04-13 00:08:38 -0300

    ver detalles componenttes

--- Crud/src/App.svelte
+++ Crud/src/App.svelte
@@ -1,17 +1,24 @@
 <script>
+  import Detalles from "./components/variables.svelte";
   export let name;
-  let ListPersonas = [
-  ];
+  let ListPersonas = [];
   let Persona = {
     id: 0,
     nombre: "",
     apellido: "",
     edad: "",
   };
-  let editarPerosnaEstado = false;
 
-  const agregarPersona = (e)=>{
-	e.preventDefault();
+  let PersonaDetail = {
+    id: 0,
+    nombre: "",
+    apellido: "",
+    edad: "",
+  };
+  let editarPerosnaEstado = false;
+  let verPersona = false;
+  const agregarPersona = (e) => {
+    e.preventDefault();
     const newPersona = {
       id: ListPersonas.length,
       nombre: Persona.nombre,
@@ -21,31 +28,29 @@
     ListPersonas = ListPersonas.concat(newPersona);
     clearPerosna();
     console.log(Persona);
-  }
+  };
 
-  const EditarPersona = (e)=>{
-	const PersonaEditar = {
+  const EditarPersona = (e) => {
+    const PersonaEditar = {
       id: Persona.id,
       nombre: Persona.nombre,
       apellido: Persona.apellido,
       edad: Persona.edad,
     };
-	console.log(Persona.id)
-	const PesonaIndex = ListPersonas.findIndex(p => p.id === Persona.id);
-	console.log(PesonaIndex);
-	ListPersonas[PesonaIndex] = PersonaEditar;
-	clearPerosna();
-	editarPerosnaEstado = false;
-  }
+    console.log(Persona.id);
+    const PesonaIndex = ListPersonas.findIndex((p) => p.id === Persona.id);
+    console.log(PesonaIndex);
+    ListPersonas[PesonaIndex] = PersonaEditar;
+    clearPerosna();
+    editarPerosnaEstado = false;
+  };
   const EscuhaPersona = (e) => {
-	e.preventDefault();
-	if (!editarPerosnaEstado) {
-		agregarPersona(e);
-	} else {
-		EditarPersona(e);
-
-	}
-	
+    e.preventDefault();
+    if (!editarPerosnaEstado) {
+      agregarPersona(e);
+    } else {
+      EditarPersona(e);
+    }
   };
 
   const clearPerosna = () => {
@@ -63,6 +68,13 @@
   const editarPerosna = (personaEditable) => {
     Persona = personaEditable;
     editarPerosnaEstado = true;
+  };
+
+  const detallesPersona = (personaDetalles) => {
+    verPersona = true;
+    
+    PersonaDetail = personaDetalles;
+    return PersonaDetail;
   };
 </script>
 
@@ -119,46 +131,54 @@
           {/if}
         </form>
       </div>
-	  {#if ListPersonas.length > 0}
-	  <div class="col-8">
-        <table class="table table-striped  table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">#id</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Apellido</th>
-              <th scope="col">Edad</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {#each ListPersonas as item}
+      {#if ListPersonas.length > 0}
+        <div class="col-8">
+          <table class="table table-striped  table-bordered">
+            <thead>
               <tr>
-                <th scope="row">{item.id}</th>
-                <td>{item.nombre}</td>
-                <td>{item.apellido}</td>
-                <td>{item.edad}</td>
-                <td>
-                  <button
-                    type="button"
-                    class="btn btn-danger"
-                    on:click={deletePerosna(item.id)}>Eliminar</button
-                  >
-                  <button
-                    type="button"
-                    class="btn btn-warning"
-                    on:click={editarPerosna(item)}
-                  >
-                    Editar</button
-                  >
-                </td>
+                <th scope="col">#id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Edad</th>
+                <th />
               </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-	  {/if}
-     
+            </thead>
+            <tbody>
+              {#each ListPersonas as item}
+                <tr>
+                  <th scope="row">{item.id}</th>
+                  <td>{item.nombre}</td>
+                  <td>{item.apellido}</td>
+                  <td>{item.edad}</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      on:click={deletePerosna(item.id)}>Eliminar</button
+                    >
+                    <button
+                      type="button"
+                      class="btn btn-warning"
+                      on:click={editarPerosna(item)}
+                    >
+                      Editar</button
+                    >
+                    <button
+                      type="button"
+                      class="btn btn-warning"
+                      on:click={detallesPersona(item)}
+                    >
+                      detalles</button
+                    >
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+      {/if}
+
+      <Detalles {PersonaDetail} bind:verPersona={verPersona} />
     </div>
   </div>
 </main>
---
+++ Crud/src/components/variables.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let PersonaDetail = {
+    id: 0,
+    nombre: "",
+    apellido: "",
+    edad: "",
+  };
+  export let verPersona;
+  
+</script>
+
+<main>
+  {#if verPersona}
+    <div class="row justify-content-center">
+      <div class=" col-md-5 ">
+        <ul class="list-group">
+          <li class="list-group-item active">Detalles de persona</li>
+          <li class="list-group-item">id:{PersonaDetail.id}</li>
+          <li class="list-group-item">nombre: {PersonaDetail.nombre}</li>
+          <li class="list-group-item">apellido: {PersonaDetail.apellido}</li>
+          <li class="list-group-item">edad: {PersonaDetail.edad}</li>
+        </ul>
+        <br />
+        <button
+          class="btn btn-primary"
+          on:click={() => (verPersona = false)}
+        >
+          cerrar
+        </button>
+      </div>
+    </div>
+  {/if}
+</main>
